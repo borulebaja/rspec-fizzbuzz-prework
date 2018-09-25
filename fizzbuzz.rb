c0ae58793486109b29e930f2555af6d4d07c4e76
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-25 06:03:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(int)
 end
 
 def fizzbuzz(int)
-  if int % 5 == 0 # if the number int is divisible by 5
-    "Buzz" # Go buzz
+  if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 5
+    "FizzBuzz" # Go buzz
   end
 end
